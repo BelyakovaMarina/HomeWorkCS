ee04f3f7f5c0ccc486eec333edea1e03d6849cc3
Marina <m_bogatyrenko@mail.ru>
2022-11-20 15:35:06 +0600

    Home work 2

---
+++ HW2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача10. Написать программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int SecondNum(int number)
+{
+    int two_digit = number % 100;
+    int new_num = two_digit / 10;
+    return new_num;
+}
+
+Console.WriteLine("Input your 3digit number: ");
+int new_num = Convert.ToInt32(Console.ReadLine());
+int res = SecondNum(new_num);
+Console.WriteLine($"Second digit of your number is: " + res);
+
+
+
+
+
+
+
