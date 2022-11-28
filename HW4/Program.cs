ca052d4ecc084ab5a41be766dfaeb2bf7c7b3beb
Marina <m_bogatyrenko@mail.ru>
2022-11-28 12:39:12 +0600

    HW4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -2,7 +2,7 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-
+/*
 int Exponentiation(int numberA, int numberB)
 {
   int result = 1;
@@ -20,3 +20,54 @@ int Exponentiation(int numberA, int numberB)
 
   int exponentiation = Exponentiation(numberA, numberB);
   Console.WriteLine("Ответ: " + exponentiation);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN)
+  {
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
